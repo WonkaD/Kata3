09e603782ad21f447399db3d70ffeb7919932d8c
WonkaD <WonkaD@10.230.170.223>
2015-10-15 09:46:44 +0100

    Kata 0.1

--- src/kata3/Main.java
+++ src/kata3/Main.java
@@ -4,7 +4,8 @@ package kata3;
 public class Main {
 
     public static void main(String[] args) {
-        
+        HistogramDisplay histogramDisplay = new HistogramDisplay("Histrograma");
+        histogramDisplay.execute();
     }
 
 }
